db9a41fcfcb6a988221aeb0eb19a3e02eb1444d1
Eric Halverson <ericleifhalverson@gmail.com>
2024-01-30 13:09:46 -0800

    Add exercises for small problems

--- small_problems/easy_one/bannerizer.rb
+++ small_problems/easy_one/bannerizer.rb
@@ -35,15 +35,25 @@ Algorithm:
 =end
 
 def print_in_box(str)
-  box_long = '+-' + ('-' * str.length) + '-+'
-  padding = '|' + (' ' * (str.length + 2)) + '|'
-  message = '| ' + str + ' |'
+  str_80 = str.slice(0..77) # Further Exploration
+                            # Truncate box so string and box are max 80 chars.
+  if str.size > 78
+    str_wrap = str.insert(78, "\n")
+  end
+
+  box_long = '+-' + ('-' * str_80.length) + '-+'
+  padding = '|' + (' ' * (str_80.length + 2)) + '|'
+
+  message = str.size > 78 ? '| ' + str_wrap + ' |' : '| ' + str + ' |'
 
   puts box_long
   puts padding
   puts message
   puts padding
   puts box_long
+
+  puts str.size
+  puts str_80.size
 end  
 
 
@@ -52,4 +62,6 @@ end
 puts print_in_box('To boldly go where no one has gone before.')
 puts
 puts print_in_box('')
+puts
+puts print_in_box('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 
